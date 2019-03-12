a403aab45e05bfd21eca45d175d88fa9ac994b87
erharmon <emerson.harmon@gmail.com>
2019-03-12 01:11:29 -0400

    the core components complete! will our hero craft a masterful vaporwave web app or will feature creep swallow the project alive? tune in next time to find out!

--- Assets/JavaScript/game.js
+++ Assets/JavaScript/game.js
@@ -1,26 +1,28 @@
 
-// CODE WIN CONDITION NEXT
+// Maybe code different levels of difficulty
 
 // establishing global variables to be used later
 var hangmanBank = ["aesthetic" , "vaporwave" , "nostalgia" , "consume", "music"];
+var bankUsed = 1;
 var userPresses = 0;
 var lettersGuessed = [];
-var hangmanWord = hangmanBank[Math.floor(Math.random() * hangmanBank.length)];
-var userGuesses = Math.floor(hangmanWord.length + (hangmanWord.length / 3));
+var hangmanWord;
+var userGuesses;
+var correctLetters = 0;
 
-
-
-// establishing a way to reset the word in play
-function newWord() {
-    var hangmanWord = hangmanBank[Math.floor(Math.random() * hangmanBank.length)];
+function generateWord() {
+    hangmanWord = hangmanBank[Math.floor(Math.random() * hangmanBank.length)];
+    userGuesses = Math.floor(hangmanWord.length + (hangmanWord.length / 3));
 }
 
 // code to be run upon the loading of the web app. this will pick a word, create a new array the length of the word 
 // picked, fill in the array with underscores, and fill in the predefined areas with the
 // guesses the player has left and the new array. 
 // also logs relevant variables to the console to make sure things are working properly. 
-(function () {
-    newWord();
+function setUp() {
+    generateWord();
+    console.log(hangmanWord);
+
     guessesLeft.textContent = userGuesses;
     
    
@@ -32,20 +34,62 @@ function newWord() {
     }
     display = hangmanLetters.join(" ");
     gameArea.textContent = display;
-    console.log(hangmanWord);
-    console.log(userGuesses);
-    console.log(hangmanLetters);
-    
-} ) ();
+}
 
-// This here is the real core of the code . when the user presses a letter, and ONLY a letter, this code will record the letter,
+function success() {
+setUp();
+console.log(hangmanBank);
+
+hangmanWord.toUpperCase();
+hangmanLetters = hangmanWord.split("");
+
+for (i = 0; i < hangmanLetters.length; i++) {
+   hangmanLetters.pop();
+   hangmanLetters.unshift("_");    
+}
+
+display = hangmanLetters.join(" ");
+gameArea.textContent = display;
+
+lettersGuessed = [];
+lettersGuessedDisplay = lettersGuessed;
+lettersGuessedDisplay.join(",");
+guessArea.textContent = lettersGuessedDisplay;
+
+userGuesses = Math.floor(hangmanWord.length + (hangmanWord.length / 3));
+guessesLeft.textContent = userGuesses;
+}
+
+function failure() {
+    setUp();
+
+    hangmanWord.toUpperCase();
+    hangmanLetters = hangmanWord.split("");
+    for (i = 0; i < hangmanLetters.length; i++) {
+       hangmanLetters.pop();
+       hangmanLetters.unshift("_");    
+    }
+    display = hangmanLetters.join(" ");
+    gameArea.textContent = display;
+
+    lettersGuessed = [];
+    lettersGuessedDisplay = lettersGuessed;
+    lettersGuessedDisplay.join(",");
+    guessArea.textContent = lettersGuessedDisplay;
+
+    userGuesses = Math.floor(hangmanWord.length + (hangmanWord.length / 3));
+    guessesLeft.textContent = userGuesses; 
+}
+// This here is the real core of the game. when the user presses a letter, and ONLY a letter, this code will record the letter,
 // write it into an array of guessed letters, increase the number of presses to keep the array in order,  and decrease the number of guesses left.
 // The code will then scan the word in play letter for letter and if the player's guess is in fact a letter in the word, it will replace the appropriate underscore(s)
 // in the hangmanLetters array, and the change will be displayed on the screen. The decrease in guesses left will also be displayed. 
 // The toUpperCase business is for aesthetic value only and may be removed when the CSS is in place. 
+
 document.onkeydown = function() {
         
     var userLetter = event.key;
+    var correctLetters = 0;
     
     display.split("");
 
@@ -75,32 +119,28 @@ document.onkeydown = function() {
     display = hangmanLetters.join(" ");
     gameArea.textContent = display;
     guessesLeft.textContent = userGuesses;
+    
+    for (i = 0; i < hangmanLetters.length; i++) {
+        if (hangmanLetters[i] === hangmanWord.charAt(i)) {
+            correctLetters++
+        }
+    }
+    
 
     // this if statement is the word reset condition. I will add some "and" requirements later to reflect wins and losses.
-    if (userGuesses === 0)  {
-        newWord();
-        console.log(hangmanWord);
-
-        hangmanWord.toUpperCase();
-        hangmanLetters = hangmanWord.split("");
-        for (i = 0; i < hangmanLetters.length; i++) {
-           hangmanLetters.pop();
-           hangmanLetters.unshift("_");    
-        }
-        display = hangmanLetters.join(" ");
-        gameArea.textContent = display;
-
-        lettersGuessed = [];
-        lettersGuessedDisplay = lettersGuessed;
-        lettersGuessedDisplay.join(",");
-        guessArea.textContent = lettersGuessedDisplay;
-
-        userGuesses = Math.floor(hangmanWord.length + (hangmanWord.length / 3));
-        guessesLeft.textContent = userGuesses;
-    }
-
+    if (userGuesses === 0) {
+        failure();
+    } 
+    
+    if (correctLetters === hangmanWord.length) {
+       console.log(hangmanBank.indexOf(hangmanWord)) 
+       hangmanBank.splice(hangmanBank.indexOf(hangmanWord), 1);
+       success();
+    }  
 }  
 
+
+setUp();
 
 
 
