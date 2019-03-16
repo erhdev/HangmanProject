d04b8a4ebbf050d2000fc77ab0787b2223b22ecd
erharmon <emerson.harmon@gmail.com>
2019-03-16 16:11:28 -0400

    all the fancy stuff done just add CSS

--- Assets/JavaScript/game.js
+++ Assets/JavaScript/game.js
@@ -1,3 +1,40 @@
+function nameEntry() {
+var usernameArr = [];
+var username;
+var isText = false;
+var namePresses = 0;
+
+
+
+ document.onkeydown = function() {
+    
+    nameLetter = event.key;
+
+    if ((nameLetter === "q") || (nameLetter === "w") || (nameLetter === "e") || (nameLetter === "r") || (nameLetter === "t") ||
+    (nameLetter === "y") || (nameLetter === "u") || (nameLetter === "i") || (nameLetter === "o") || (nameLetter === "p") ||
+    (nameLetter === "a") || (nameLetter === "s") || (nameLetter === "d") || (nameLetter === "f") || (nameLetter === "g") ||
+    (nameLetter === "h") || (nameLetter === "j") || (nameLetter === "k") || (nameLetter === "l") || (nameLetter === "z") ||
+    (nameLetter === "x") || (nameLetter === "c") || (nameLetter === "v") || (nameLetter === "b") || (nameLetter === "n") || 
+    (nameLetter === "m") || (nameLetter === "1") || (nameLetter === "2") || (nameLetter === "3") || (nameLetter === "4") || 
+    (nameLetter === "5") || (nameLetter === "6") || (nameLetter === "7") || (nameLetter === "8") || (nameLetter === "9") || 
+    (nameLetter === "0") || (nameLetter === "!") || (nameLetter === "@") || (nameLetter === "#") || (nameLetter === "$") || 
+    (nameLetter === "%") || (nameLetter === "^") || (nameLetter === "&") || (nameLetter === "*") || (nameLetter === "-") || 
+    (nameLetter === "_") || (nameLetter === "+") || (nameLetter === "=") || (nameLetter === "?") || (nameLetter === ".") || 
+    (nameLetter === ",") || (nameLetter === "/")  ) {
+        namePresses++
+        usernameArr.splice(namePresses, 0, nameLetter);
+        isText = true;
+    }
+    username = usernameArr.join("");
+    console.log(username);
+    console.log(nameLetter);
+    console.log(namePresses);
+    console.log(isText);
+    whatisname.textContent = username;
+    
+ }
+}    
+function runGame() {
 // establishing global variables to be used later
 
 // these variables all relate to the level the user is on and what words they have to guess
@@ -65,8 +102,27 @@ function setUp() {
     wordArea.textContent = display;
     wordsLeft.textContent = bankInUse.length;
 }
-
+// success and failure functions. these run the setUp function and other stuff depending on whether the user succeeds or fails
+// conditions for success and failure found below. 
 function success() {
+    
+       // this chunk of code will add the guessed word to the array wordsCorrect, which then gets
+       // displayed to the user
+
+       wordsCorrect[numWordsCorrect] = ("  " + hangmanWord + "  ");
+       numWordsCorrect++
+       wordsGuessed.textContent = wordsCorrect;
+       score.textContent = numWordsCorrect;
+
+       // this line deletes the word that was guessed from the bank, which ensures there are no repeats
+       bankInUse.splice(bankInUse.indexOf(hangmanWord), 1);
+
+        // this if statement will move the user to the next level if they have completed all the words
+        // on the current level
+
+       if (bankInUse.length === 0) {
+           bankUsed++
+       }
 setUp();
 console.log(bankInUse);
 
@@ -108,6 +164,7 @@ function failure() {
     userGuesses = Math.floor(hangmanWord.length + (hangmanWord.length / 3));
     guessesLeft.textContent = userGuesses;     
 }
+
 // This here is the real core of the game. when the user presses a letter, and ONLY a letter, this code will record the letter,
 // write it into an array of guessed letters, increase the number of presses to keep the array in order,  and decrease the number of guesses left.
 // The code will then scan the word in play letter for letter and if the player's guess is in fact a letter in the word, it will replace the appropriate underscore(s)
@@ -136,21 +193,18 @@ document.onkeydown = function() {
                 // as the user's guess, it changes a boolean to true.
                 if (userPresses > 1) {
 
-                for (var i = 0; i < userPresses; i++) {if (userLetter === lettersGuessed[i]) {repeatLetter = true;}} 
+                for (var i = 0; i < userPresses; i++) {if ("  " + userLetter  + "  "=== lettersGuessed[i]) {repeatLetter = true;}} 
                 
                 } else {
-                    lettersGuessed.splice(userPresses, 0, userLetter);
+                    lettersGuessed.splice(userPresses, 0, "  " + userLetter + "  ");
                     userGuesses--
                 }
                 // if that boolean is true, this array will not make sure the guess is not added to the array. the userPresses > 1 condition is there
                 // because at userPresses = 0 the key will get logged twice without that condition. 
                 if ((repeatLetter !== true) &&  (userPresses > 1 )) {
-                    lettersGuessed.splice(userPresses, 0, userLetter);
+                    lettersGuessed.splice(userPresses, 0, "  " + userLetter  + "  ");
                     userGuesses--
                 } 
-              
-            
-                repeatLetter = false;
         }
     
     
@@ -181,32 +235,12 @@ document.onkeydown = function() {
     } 
     
     if (correctLetters === hangmanWord.length) {
-    
-       // this chunk of code will add the guessed word to the array wordsCorrect, which then gets
-       // displayed to the user
-
-       wordsCorrect[numWordsCorrect] = (" " + hangmanWord + " ");
-       numWordsCorrect++
-       wordsGuessed.textContent = wordsCorrect;
-
-       // this line deletes the word that was guessed from the bank, which ensures there are no repeats
-       bankInUse.splice(bankInUse.indexOf(hangmanWord), 1);
-
-        // this if statement will move the user to the next level if they have completed all the words
-        // on the current level
-
-       if (bankInUse.length === 0) {
-           bankUsed++
-       }
-
-       success();
+        success();
     }  
 }  
 
 // call setUp to make sure there's something on screen for the user
 setUp();
+//fin
+}
 
-
-
-
-// flavor
